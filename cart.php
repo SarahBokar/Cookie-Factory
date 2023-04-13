45e57e2aa5d92b8f20a1e64b6f7e9d23a1af0841
Sarahbokar <bokarthiresarah@gmail.com>
2023-04-13 09:45:19 +0200

    cart et index modifiés

--- cart.php
+++ cart.php
@@ -1,7 +1,25 @@
 <?php require 'inc/head.php'; ?>
 <section class="cookies container-fluid">
     <div class="row">
-        TODO : Display shopping cart items from $_SESSION here.
+
+
+        <div class="col-xs-12">
+            <h1>Carte</h1>
+            <?php if (count($_SESSION['cart']) == 0) { ?>
+            <p>Your cart is empty.</p>
+            <?php } else { ?>
+            <ul>
+                <?php foreach ($_SESSION['cart'] as $id => $cookie) { ?>
+                <li><?= $cookie['name']; ?></li>
+                <?php } ?>
+            </ul>
+            <?php } ?>
+        </div>
+git
+
     </div>
 </section>
+
+
+
 <?php require 'inc/foot.php'; ?>
